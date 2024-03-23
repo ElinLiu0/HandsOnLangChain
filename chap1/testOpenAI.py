4f0e80cf26523e367ad7b8f9c9b4ee8b29f4f45a
elin <982467922@qq.com>
2024-03-23 22:30:57 +0800

    CallBackProcessor

--- chap1/testOpenAI.py
+++ chap1/testOpenAI.py
@@ -1,16 +1,17 @@
 import openai
-openai.api_base = "http://localhost:8000/v1"
-openai.api_key = "none"
+from openai import OpenAI
+
+client = OpenAI(api_key="none")
+# TODO: The 'openai.api_base' option isn't read in the client API. You will need to pass it when you instantiate the client, e.g. 'OpenAI(base_url="http://localhost:8000/v1")'
+# openai.api_base = "http://localhost:8000/v1"
 
 # 使用流式回复的请求
-for chunk in openai.ChatCompletion.create(
-    model="Qwen/Qwen1.5-4B-Chat",
-    messages=[
-        {"role": "user", "content": "You know who the Pat Gelsinger is?"}
-    ],
-    stream=True
-    # 流式输出的自定义stopwords功能尚未支持，正在开发中
-):
+for chunk in client.chat.completions.create(model="Qwen/Qwen1.5-4B-Chat",
+messages=[
+    {"role": "user", "content": "You know who the Pat Gelsinger is?"}
+],
+stream=True
+# 流式输出的自定义stopwords功能尚未支持，正在开发中):
     if hasattr(chunk.choices[0].delta, "content"):
         print(chunk.choices[0].delta.content, end="", flush=True)
 
